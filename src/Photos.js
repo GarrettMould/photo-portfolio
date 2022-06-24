7d1adac78581542185abd864f2ddc7fb7f62d75c
Garrett Mould <garrettmould@gmail.com>
2022-06-24 17:52:39 -0500

    photo upload

--- src/Photos.js
+++ src/Photos.js
@@ -60,12 +60,62 @@ import NhaTrang35 from "../src/images/NhaTrang/R1-03344-0034.JPG";
 import NhaTrang36 from "../src/images/NhaTrang/R1-03344-0035.JPG";
 import NhaTrang37 from "../src/images/NhaTrang/R1-03344-0036.JPG";
 
+import DaNang1 from '../src/images/DaNang/000015.jpg'
+import DaNang2 from '../src/images/DaNang/000016.jpg'
+import DaNang3 from '../src/images/DaNang/000017.jpg'
+import DaNang4 from '../src/images/DaNang/000018.jpg'
+import DaNang5 from '../src/images/DaNang/000019.jpg'
+import DaNang6 from '../src/images/DaNang/000020.jpg'
+import DaNang7 from '../src/images/DaNang/000021.jpg'
+import DaNang8 from '../src/images/DaNang/000022.jpg'
+import DaNang9 from '../src/images/DaNang/000023.jpg'
+import DaNang10 from '../src/images/DaNang/000024.jpg'
+import DaNang11 from '../src/images/DaNang/000025.jpg'
+import DaNang12 from '../src/images/DaNang/000026.jpg'
+import DaNang13 from '../src/images/DaNang/000027.jpg'
+import DaNang14 from '../src/images/DaNang/000028.jpg'
+import DaNang15 from '../src/images/DaNang/000029.jpg'
+import DaNang16 from '../src/images/DaNang/000030.jpg'
+import DaNang17 from '../src/images/DaNang/000031.jpg'
+import DaNang18 from '../src/images/DaNang/000032.jpg'
+import DaNang19 from '../src/images/DaNang/000033.jpg'
+import DaNang20 from '../src/images/DaNang/000034.jpg'
+import DaNang21 from '../src/images/DaNang/000035.jpg'
+import DaNang22 from '../src/images/DaNang/000036.jpg'
+import DaNang23 from '../src/images/DaNang/000037.jpg'
+import DaNang24 from '../src/images/DaNang/000038.jpg'
+import DaNang25 from '../src/images/DaNang/000039.jpg'
+import DaNang26 from '../src/images/DaNang/000040.jpg'
+import DaNang27 from '../src/images/DaNang/000041.jpg'
+
+import Hanoi1 from '../src/images/Hanoi/000039.jpg'
+import Hanoi2 from '../src/images/Hanoi/000041.jpg'
+import Hanoi3 from '../src/images/Hanoi/000042.jpg'
+import Hanoi4 from '../src/images/Hanoi/000043.jpg'
+import Hanoi5 from '../src/images/Hanoi/000044.jpg'
+import Hanoi6 from '../src/images/Hanoi/000045.jpg'
+import Hanoi7 from '../src/images/Hanoi/000046.jpg'
+import Hanoi8 from '../src/images/Hanoi/000047.jpg'
+import Hanoi9 from '../src/images/Hanoi/000048.jpg'
+import Hanoi10 from '../src/images/Hanoi/000049.jpg'
+import Hanoi11 from '../src/images/Hanoi/000050.jpg'
+import Hanoi12 from '../src/images/Hanoi/000051.jpg'
+import Hanoi13 from '../src/images/Hanoi/000052.jpg'
+import Hanoi14 from '../src/images/Hanoi/000053.jpg'
+import Hanoi15 from '../src/images/Hanoi/000054.jpg'
+import Hanoi16 from '../src/images/Hanoi/000055.jpg'
+import Hanoi17 from '../src/images/Hanoi/000057.jpg'
+import Hanoi18 from '../src/images/Hanoi/000058.jpg'
+import Hanoi19 from '../src/images/Hanoi/000059.jpg'
+import Hanoi20 from '../src/images/Hanoi/000060.jpg'
+
+
 export var photos = [
   {
     id: 0,
     destination: "Nha Trang & Singapore",
     dates: "June 8th - June 15th",
-    summary: "This trip included an eventful three-day vacation to Nha Trang, as well as my first trip overseas since before the pandemic. I saw a bit of everything, from beaches, to waterfalls, to the impressive Singaporean architecture.",
+    summary: "This trip included an eventful three-day vacation to Nha Trang, as well as my first trip overseas since before the pandemic. I saw a bit of everything, from beaches, to waterfalls, to the impressive Singaporean architecture. A fantastic trip with plenty of good memories.",
     cameraTitle: "Kyocera Zoomtec 80",
     filmTitle: "Fuji Superia 400",
     camera: KyoceraZoomtec80,
@@ -262,7 +312,7 @@ export var photos = [
     id: 1,
     destination: "Pu Luong",
     dates: "March 18th - March 20th",
-    summary: "This trip included an eventful three-day vacation to Nha Trang, as well as my first trip overseas since before the pandemic. I saw a bit of everything, from beaches, to waterfalls, to the impressive Singaporean architecture.",
+    summary: "Pu Luong is one of my favorite getaways in Vietnam. Not many people know about it, so if you travel on the right weekend, you have the winding mountain roads all to yourself. Don't be fooled by the bad photos, the waterfalls are much more stunning in person.",
     cameraTitle: "Kyocera Zoomtec 80",
     filmTitle: "Kodak Gold 200",
     camera: KyoceraZoomtec80,
@@ -369,39 +419,144 @@ export var photos = [
     id: 2,
     destination: "Da Nang / Hoi An",
     dates: "February 2nd - February 5th",
-    summary: "This trip included an eventful three-day vacation to Nha Trang, as well as my first trip overseas since before the pandemic. I saw a bit of everything, from beaches, to waterfalls, to the impressive Singaporean architecture.",
+    summary: "These photos are from a Tet Holiday trip to Da Nang and Hoi An. Not a lot of beach action in Da Nang due to the crummy weather, but plenty of exploring a lots of good pictures.",
     cameraTitle: "Canon AutoBoy Luna XL",
     filmTitle: "Kodak Gold 200",
     camera: CanonAutoboy,
     film: KodakGold200,
     photos: [
       {
-        src: PuLuong1,
+        src: DaNang1,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong2,
+        src: DaNang2,
         width: 1,
         height: 1,
       },
       {
-        src: PuLuong3,
+        src: DaNang3,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong4,
+        src: DaNang4,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong5,
+        src: DaNang5,
         width: 1,
         height: 1,
       },
       {
-        src: PuLuong6,
+        src: DaNang6,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang7,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang8,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang9,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang10,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang11,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang12,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang13,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang14,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang15,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang16,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang17,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang18,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang19,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang20,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang21,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang22,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang23,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang24,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang25,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: DaNang26,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: DaNang27,
         width: 4,
         height: 3,
       },
@@ -411,42 +566,113 @@ export var photos = [
     id: 3,
     destination: "Random Hanoi Photos",
     dates: "January 2022",
-    summary: "This trip included an eventful three-day vacation to Nha Trang, as well as my first trip overseas since before the pandemic. I saw a bit of everything, from beaches, to waterfalls, to the impressive Singaporean architecture.",
+    summary: "These photos were taken at various places around Hanoi during a weekend of exploration. This was the first roll of film I had purchased in quite some time, and this marks the time that I got back into photography.",
     cameraTitle: "Canon AutoBoy Luna XL",
     filmTitle: "Fuji Superia 400",
     camera: CanonAutoboy,
     film: FujiSuperia400,
     photos: [
       {
-        src: PuLuong1,
+        src: Hanoi1,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong2,
+        src: Hanoi2,
         width: 1,
         height: 1,
       },
       {
-        src: PuLuong3,
+        src: Hanoi3,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong4,
+        src: Hanoi4,
         width: 4,
         height: 3,
       },
       {
-        src: PuLuong5,
+        src: Hanoi5,
         width: 1,
         height: 1,
       },
       {
-        src: PuLuong6,
+        src: Hanoi6,
         width: 4,
         height: 3,
       },
+      {
+        src: Hanoi7,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi8,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: Hanoi9,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi10,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi11,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: Hanoi12,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi13,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi14,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: Hanoi15,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi16,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi17,
+        width: 1,
+        height: 1,
+      },
+      {
+        src: Hanoi18,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi19,
+        width: 4,
+        height: 3,
+      },
+      {
+        src: Hanoi20,
+        width: 1,
+        height: 1,
+      },
+      
     ],
   },
 ];
