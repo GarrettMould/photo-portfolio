eaa82464180a68fce22c4443df163912669d1dbb
Garrett Mould <garrettmould@gmail.com>
2022-06-29 13:01:47 -0500

    style overhaul

--- src/components/SelectedTrip.js
+++ src/components/SelectedTrip.js
@@ -2,7 +2,7 @@ import { StyledContainer } from "./styles/Container.styles";
 
 import SelectedTripDetails from "../elements/SelectedTripDetails";
 
-import SelectedTripSummary from "../elements/SelectedTripSummary";
+import SelectedTripSummary from "../Old Stuff/SelectedTripSummary";
 
 import SelectedTripGallery from "../elements/SelectedTripGallery";
 
