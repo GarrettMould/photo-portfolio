1e1a7e640d5b386b61e14995122b88c0e43806ba
Garrett Mould <garrettmould@gmail.com>
2022-07-07 12:06:57 -0500

    added mobile styling

---
+++ src/components/GridSpacerMobileFour.js
@@ -0,0 +1,12 @@
+import {
+  GridContainer,
+  GridRow,
+  GridColumn,
+  Image,
+} from "./styles/StyledGridSection.styled";
+
+const GridSpacerMobileFour = (props) => {
+  return <div></div>;
+};
+
+export default GridSpacerMobileFour;
