742850606130199a70199a3508bb24f391e728b6
Garrett Mould <garrettmould@gmail.com>
2022-07-30 13:24:52 +0700

    handle image select (pop up modal)

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,17 +22,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = (props) => { 
 
   const [tripIndex, setTripIndex] = useState(0)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalSource, setModalSource] = useState(null);
 
   
+  console.log(tripIndex)
+  console.log(modalOpen)
+  
+  const handleTripSelect = (index) => { 
+    setTripIndex(index)
+  };
 
+  const handleImageSelect = (e) => { 
+    console.log(e.currentTarget.src); 
+    setModalOpen(true);
+    setModalSource(e.currentTarget.src);
+  }
+
+  const closeModal = () => { 
+    setModalOpen(false);
+  }
   
 
   return (
     <StyledContainer>
-      <HeaderNew></HeaderNew>
+      <HeaderNew closeModal={closeModal}></HeaderNew>
       <Routes>
-        <Route path="/" element={<HomePage tripIndex={tripIndex}></HomePage>}/>
-        <Route path="/SelectedTrip" element={<SelectedTrip tripIndex={tripIndex}></SelectedTrip>}/>
+        <Route path="/" element={<HomePage tripIndex={tripIndex} handleTripSelect={handleTripSelect}></HomePage>}/>
+        <Route path="/SelectedTrip" element={<SelectedTrip tripIndex={tripIndex} modalOpen={modalOpen} handleImageSelect={handleImageSelect} modalSource={modalSource}></SelectedTrip>}/>
       </Routes>
       
       {/*<HomePage></HomePage>*/}
